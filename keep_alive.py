d83a85c24313b345cd72e2c7b2f03b5f1e9c4eb7
spellbinderyar <spellbinderyardirhan@gmail.com>
2025-07-06 12:29:04 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,22 @@
+from flask import Flask
+from threading import Thread
+import os
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "CoinTracker botu çalışıyor."
+
+
+def run():
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Flask sunucusu başlatılıyor... Port: {port}")
+    app.run(host="0.0.0.0", port=port)
+
+
+def keep_alive():
+    print("keep_alive() fonksiyonu çağrıldı.")
+    t = Thread(target=run)
+    t.start()
